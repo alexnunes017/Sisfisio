53b88c998c9cec9a536a018949218baf1abbd539
nunes <nunes@JNA>
2016-11-14 07:57:16 -0200

    Alteracao

--- src/main/java/br/com/nunes/fisioterapia/bean/PessoaBean.java
+++ src/main/java/br/com/nunes/fisioterapia/bean/PessoaBean.java
@@ -1,15 +1,20 @@
 package br.com.nunes.fisioterapia.bean;
 
 import java.io.Serializable;
+import java.sql.Connection;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.event.ActionEvent;
 
+import org.omnifaces.util.Faces;
 import org.omnifaces.util.Messages;
+import org.primefaces.component.datatable.DataTable;
 
 import br.com.nunes.fisioterapia.dao.CidadeDAO;
 import br.com.nunes.fisioterapia.dao.EstadoDAO;
@@ -17,150 +22,204 @@ import br.com.nunes.fisioterapia.dao.PessoaDAO;
 import br.com.nunes.fisioterapia.domain.Cidade;
 import br.com.nunes.fisioterapia.domain.Estado;
 import br.com.nunes.fisioterapia.domain.Pessoa;
+import br.com.nunes.fisioterapia.util.HibernateUtil;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.view.JasperViewer;
 
 @SuppressWarnings("serial")
 @ManagedBean
 @ViewScoped
 public class PessoaBean implements Serializable {
-	private Pessoa pessoa;
-	private List<Pessoa> pessoas;
+  private Pessoa pessoa;
+  private List<Pessoa> pessoas;
 
-	private Estado estado;
-	private List<Estado> estados;
+  private Estado estado;
+  private List<Estado> estados;
 
-	private List<Cidade> cidades;
+  private List<Cidade> cidades;
 
-	public Pessoa getPessoa() {
-		return pessoa;
-	}
+  public Pessoa getPessoa() {
+    return pessoa;
+  }
 
-	public void setPessoa(Pessoa pessoa) {
-		this.pessoa = pessoa;
-	}
+  public void setPessoa(Pessoa pessoa) {
+    this.pessoa = pessoa;
+  }
 
-	public List<Pessoa> getPessoas() {
-		return pessoas;
-	}
+  public List<Pessoa> getPessoas() {
+    return pessoas;
+  }
 
-	public void setPessoas(List<Pessoa> pessoas) {
-		this.pessoas = pessoas;
-	}
+  public void setPessoas(List<Pessoa> pessoas) {
+    this.pessoas = pessoas;
+  }
 
-	public Estado getEstado() {
-		return estado;
-	}
+  public Estado getEstado() {
+    return estado;
+  }
 
-	public void setEstado(Estado estado) {
-		this.estado = estado;
-	}
+  public void setEstado(Estado estado) {
+    this.estado = estado;
+  }
 
-	public List<Estado> getEstados() {
-		return estados;
-	}
+  public List<Estado> getEstados() {
+    return estados;
+  }
 
-	public void setEstados(List<Estado> estados) {
-		this.estados = estados;
-	}
+  public void setEstados(List<Estado> estados) {
+    this.estados = estados;
+  }
 
-	public List<Cidade> getCidades() {
-		return cidades;
-	}
+  public List<Cidade> getCidades() {
+    return cidades;
+  }
 
-	public void setCidades(List<Cidade> cidades) {
-		this.cidades = cidades;
-	}
+  public void setCidades(List<Cidade> cidades) {
+    this.cidades = cidades;
+  }
 
-	@PostConstruct
-	public void listar() {
-		try {
-			PessoaDAO pessoaDAO = new PessoaDAO();
-			pessoas = pessoaDAO.listar("nome");
-		} catch (RuntimeException erro) {
-			Messages.addGlobalError("Ocorreu um erro ao tentar listar as pessoas");
-			erro.printStackTrace();
-		}
-	}
+  @PostConstruct
+  public void listar() {
+    try {
+      PessoaDAO pessoaDAO = new PessoaDAO();
+      pessoas = pessoaDAO.listar("nome");
+    } catch (RuntimeException erro) {
+      Messages.addGlobalError("Ocorreu um erro ao tentar listar as pessoas");
+      erro.printStackTrace();
+    }
+  }
 
-	public void novo() {
-		try {
-			pessoa = new Pessoa();
+  public void novo() {
+    try {
+      pessoa = new Pessoa();
 
-			estado = new Estado();
+      estado = new Estado();
 
-			EstadoDAO estadoDAO = new EstadoDAO();
-			estados = estadoDAO.listar("nome");
+      EstadoDAO estadoDAO = new EstadoDAO();
+      estados = estadoDAO.listar("nome");
 
-			cidades = new ArrayList<>();
-		} catch (RuntimeException erro) {
-			Messages.addGlobalError("Ocorreu um erro ao tentar gerar uma nova pessoa");
-			erro.printStackTrace();
-		}
-	}
+      cidades = new ArrayList<>();
+    } catch (RuntimeException erro) {
+      Messages.addGlobalError("Ocorreu um erro ao tentar gerar uma nova pessoa");
+      erro.printStackTrace();
+    }
+  }
 
-	public void editar(ActionEvent evento) {
-		try {
-			pessoa = (Pessoa) evento.getComponent().getAttributes().get("pessoaSelecionada");
+  public void editar(ActionEvent evento) {
+    try {
+      pessoa = (Pessoa) evento.getComponent().getAttributes().get("pessoaSelecionada");
 
-			estado = pessoa.getCidade().getEstado();
+      estado = pessoa.getCidade().getEstado();
 
-			EstadoDAO estadoDAO = new EstadoDAO();
-			estados = estadoDAO.listar("nome");
+      EstadoDAO estadoDAO = new EstadoDAO();
+      estados = estadoDAO.listar("nome");
 
-			CidadeDAO cidadeDAO = new CidadeDAO();
-			cidades = cidadeDAO.buscarPorEstado(estado.getCodigo());
-		} catch (RuntimeException erro) {
-			Messages.addGlobalError("Ocorreu um erro ao tentar selecionar uma pessoa");
-		}
-	}
+      CidadeDAO cidadeDAO = new CidadeDAO();
+      cidades = cidadeDAO.buscarPorEstado(estado.getCodigo());
+    } catch (RuntimeException erro) {
+      Messages.addGlobalError("Ocorreu um erro ao tentar selecionar uma pessoa");
+    }
+  }
 
-	public void salvar() {
-		try {
-			PessoaDAO pessoaDAO = new PessoaDAO();
-			pessoaDAO.merge(pessoa);
+  public void salvar() {
+    try {
+      PessoaDAO pessoaDAO = new PessoaDAO();
+      pessoaDAO.merge(pessoa);
 
-			pessoas = pessoaDAO.listar("nome");
+      pessoas = pessoaDAO.listar("nome");
 
-			pessoa = new Pessoa();
+      pessoa = new Pessoa();
 
-			estado = new Estado();
+      estado = new Estado();
 
-			EstadoDAO estadoDAO = new EstadoDAO();
-			estados = estadoDAO.listar("nome");
+      EstadoDAO estadoDAO = new EstadoDAO();
+      estados = estadoDAO.listar("nome");
 
-			cidades = new ArrayList<>();
+      cidades = new ArrayList<>();
 
-			Messages.addGlobalInfo("Pessoa salva com sucesso");
-		} catch (RuntimeException erro) {
-			Messages.addGlobalError("Ocorreu um erro ao tentar salvar a pessoa");
-			erro.printStackTrace();
-		}
-	}
+      Messages.addGlobalInfo("Pessoa salva com sucesso");
+    } catch (RuntimeException erro) {
+      Messages.addGlobalError("Ocorreu um erro ao tentar salvar a pessoa");
+      erro.printStackTrace();
+    }
+  }
 
-	public void excluir(ActionEvent evento) {
-		try {
-			pessoa = (Pessoa) evento.getComponent().getAttributes().get("pessoaSelecionado");
+  public void excluir(ActionEvent evento) {
+    try {
+      pessoa = (Pessoa) evento.getComponent().getAttributes().get("pessoaSelecionado");
 
-			PessoaDAO pessoaDAO = new PessoaDAO();
-			pessoaDAO.excluir(pessoa);
-			listar();
-			Messages.addGlobalInfo("Pessoa excluido com sucesso!");
-		} catch (RuntimeException e) {
-			Messages.addGlobalError("Ocorreu um erro ao tentar excluir a pessoa");
-			e.printStackTrace();
-		}
-	}
+      PessoaDAO pessoaDAO = new PessoaDAO();
+      pessoaDAO.excluir(pessoa);
+      listar();
+      Messages.addGlobalInfo("Pessoa excluido com sucesso!");
+    } catch (RuntimeException e) {
+      Messages.addGlobalError("Ocorreu um erro ao tentar excluir a pessoa");
+      e.printStackTrace();
+    }
+  }
 
-	public void popular() {
-		try {
-			if (estado != null) {
-				CidadeDAO cidadeDAO = new CidadeDAO();
-				cidades = cidadeDAO.buscarPorEstado(estado.getCodigo());
-			} else {
-				cidades = new ArrayList<>();
-			}
-		} catch (RuntimeException erro) {
-			Messages.addGlobalError("Ocorreu um erro ao tentar filtrar as cidades");
-			erro.printStackTrace();
-		}
-	}
+  public void popular() {
+    try {
+      if (estado != null) {
+        CidadeDAO cidadeDAO = new CidadeDAO();
+        cidades = cidadeDAO.buscarPorEstado(estado.getCodigo());
+      } else {
+        cidades = new ArrayList<>();
+      }
+    } catch (RuntimeException erro) {
+      Messages.addGlobalError("Ocorreu um erro ao tentar filtrar as cidades");
+      erro.printStackTrace();
+    }
+  }
+
+  public void imprimir() {
+    try {
+      DataTable tabela = (DataTable) Faces.getViewRoot().findComponent("formListagem:tabela");
+      Map<String, Object> filtros = tabela.getFilters();
+
+      String nomeEstado = (String) filtros.get("nome");
+      String siglaEstado = (String) filtros.get("sigla");
+
+      String caminho = Faces.getRealPath("/resources/reports/rel_Estados.jasper");
+
+      // String caminhoBanner =
+      // Faces.getRealPath("/resources/images/banner2.jpg");
+
+      Map<String, Object> parametros = new HashMap<>();
+
+      if (nomeEstado == null) {
+        parametros.put("nomeEstado", "%%");
+      } else {
+        parametros.put("nomeEstado", "%" + nomeEstado + "%");
+      }
+      if (siglaEstado == null) {
+        parametros.put("siglaEstado", "%%");
+      } else {
+        parametros.put("siglaEstado", "%" + siglaEstado + "%");
+      }
+
+      // parametros.put("CAMINHO_BANNER", caminhoBanner);
+
+      Connection conexao = HibernateUtil.getConexao();
+
+      JasperPrint relatorio = JasperFillManager.fillReport(caminho, parametros, conexao);
+      JasperViewer viewer = new JasperViewer(relatorio, false);
+      if (relatorio.getAnchorIndexes().isEmpty()) {
+        System.out.println("SemPagina");
+      } else {
+
+        viewer.setTitle("Relátorio de Estado(s)");
+        viewer.setVisible(true);
+        viewer.toFront();
+      }
+      ;
+      // JasperPrintManager.printReport(relatorio, true);
+
+    } catch (JRException erro) {
+      Messages.addGlobalError("Ocorreu um erro ao tentar gerar o relatório");
+      erro.printStackTrace();
+    }
+  }
 }
--- src/main/java/br/com/nunes/fisioterapia/bean/UsuarioBean.java
+++ src/main/java/br/com/nunes/fisioterapia/bean/UsuarioBean.java
@@ -1,7 +1,10 @@
 package br.com.nunes.fisioterapia.bean;
 
 import java.io.Serializable;
+import java.sql.Connection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
@@ -9,7 +12,9 @@ import javax.faces.bean.ViewScoped;
 import javax.faces.event.ActionEvent;
 
 import org.apache.shiro.crypto.hash.SimpleHash;
+import org.omnifaces.util.Faces;
 import org.omnifaces.util.Messages;
+import org.primefaces.component.datatable.DataTable;
 
 import br.com.nunes.fisioterapia.dao.CidadeDAO;
 import br.com.nunes.fisioterapia.dao.EstadoDAO;
@@ -19,6 +24,11 @@ import br.com.nunes.fisioterapia.domain.Cidade;
 import br.com.nunes.fisioterapia.domain.Estado;
 import br.com.nunes.fisioterapia.domain.Pessoa;
 import br.com.nunes.fisioterapia.domain.Usuario;
+import br.com.nunes.fisioterapia.util.HibernateUtil;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.view.JasperViewer;
 
 @SuppressWarnings("serial")
 @ManagedBean
@@ -157,6 +167,55 @@ public class UsuarioBean implements Serializable {
     } catch (RuntimeException e) {
       Messages.addGlobalError("Ocorreu um erro ao tentar excluir a pessoa");
       e.printStackTrace();
+    }
+  }
+
+  public void imprimir() {
+    try {
+      DataTable tabela = (DataTable) Faces.getViewRoot().findComponent("formListagem:tabela");
+      Map<String, Object> filtros = tabela.getFilters();
+
+      String nomeEstado = (String) filtros.get("nome");
+      String siglaEstado = (String) filtros.get("sigla");
+
+      String caminho = Faces.getRealPath("/resources/reports/rel_Estados.jasper");
+
+      // String caminhoBanner =
+      // Faces.getRealPath("/resources/images/banner2.jpg");
+
+      Map<String, Object> parametros = new HashMap<>();
+
+      if (nomeEstado == null) {
+        parametros.put("nomeEstado", "%%");
+      } else {
+        parametros.put("nomeEstado", "%" + nomeEstado + "%");
+      }
+      if (siglaEstado == null) {
+        parametros.put("siglaEstado", "%%");
+      } else {
+        parametros.put("siglaEstado", "%" + siglaEstado + "%");
+      }
+
+      // parametros.put("CAMINHO_BANNER", caminhoBanner);
+
+      Connection conexao = HibernateUtil.getConexao();
+
+      JasperPrint relatorio = JasperFillManager.fillReport(caminho, parametros, conexao);
+      JasperViewer viewer = new JasperViewer(relatorio, false);
+      if (relatorio.getAnchorIndexes().isEmpty()) {
+        System.out.println("SemPagina");
+      } else {
+
+        viewer.setTitle("Relátorio de Estado(s)");
+        viewer.setVisible(true);
+        viewer.toFront();
+      }
+      ;
+      // JasperPrintManager.printReport(relatorio, true);
+
+    } catch (JRException erro) {
+      Messages.addGlobalError("Ocorreu um erro ao tentar gerar o relatório");
+      erro.printStackTrace();
     }
   }
 
